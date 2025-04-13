969d3d331e4ec8e03c816abe227166d3f634245e
MilesMorales2000 <164797085+MilesMorales2000@users.noreply.github.com>
2025-04-13 19:51:43 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
+  SESSION_ID: process.env.SESSION_ID || "JOEL-XMD~9dwwkIpS#oUnIWeTAmVMhqGl-jzy1X8uojKacp8S5LYS0qxqzqU4",
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
